99a96f24dad26f2a0b1b61988d8ebdd9947af11f
alexonplus <oleks9185@gmail.com>
2026-04-09 15:54:14 +0200

    Shift + Alt + F

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
     return (
         <nav style={navStyle}>
             {!isAuthenticated ? (
-                <> 
+                <>
                     <Link to="/">Events</Link>
                     <Link to="/login">Login</Link>
                     <Link to="/register">Register</Link>
